568da9ade6b5af103ef4b862828e29506abd618f
johnmart19 <johnivan19999@gmail.com>
2025-05-09 19:41:58 +0300

    apollo: Setup prebuilt audio HAL
    Drop BOARD_USES_ALSA_AUDIO as it will declare audio.primary.kona, which
    is shipped prebuilt.
    Declare TARGET_PROVIDES_AUDIO_EXTNS as we ship prebuilt liba2dpoffload.
    
    Co-Authored-By: Hridaya Prajapati <info.hridayaprajapati@gmail.com>

--- apollo-vendor.mk
+++ apollo-vendor.mk
@@ -416,7 +416,9 @@ PRODUCT_PACKAGES += \
     libadpcmdec \
     libaudcal \
     libaudio_log_utils \
+    libaudioparsers \
     libaudioroute_ext \
+    libbatterylistener \
     libbluetooth_audio_session_qti \
     libbluetooth_audio_session_qti_2_1 \
     libcapiv2svacnn \
@@ -424,11 +426,15 @@ PRODUCT_PACKAGES += \
     libcapiv2vop \
     libdrc \
     libdsd2pcm \
+    libexthwplugin \
     libfastcrc \
     libgcs-calwrapper \
     libgcs-ipc \
     libgcs-osal \
     libgcs \
+    libhdmiedid \
+    libhdmipassthru \
+    libhfp \
     liblistensoundmodel2 \
     libmm-hdcpmgr \
     libmmosal_vendor \
@@ -444,6 +450,8 @@ PRODUCT_PACKAGES += \
     libqtigef \
     libresampler \
     libsmwrapper \
+    libsndmonitor \
+    libspkrprot \
     libssrec \
     libsurround_3mic_proc \
     libwfdcodecv4l2_proprietary \
